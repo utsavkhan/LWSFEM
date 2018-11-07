52b65e2983925aba95089ee7be83ea31f567df8b
utsavkhan <38136166+utsavkhan@users.noreply.github.com>
2018-11-07 17:55:19 +0100

    change sign of P

--- elksigma.m
+++ elksigma.m
@@ -10,7 +10,7 @@ function [Kesigma]=elksigma(le,P,I0,A);
 %
 % Make sure tlee initial stress matrix is symmetric!
 
-Kesigma = -P*[36/30*le -3*le/30*le 0 -36/30*le -3*le/30*le 0;
+Kesigma = P*[36/30*le -3*le/30*le 0 -36/30*le -3*le/30*le 0;
     -3*le/30*le 4*le^2/30*le 0 3*le/30*le -le^2/30*le 0;
     0 0 I0/A*le 0 0 -I0/A*le ;
     -36/30*le 3*le/30*le 0 36/30*le 3*le/30*le 0;
